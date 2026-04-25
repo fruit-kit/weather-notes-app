62016faf5c4b1450a4ee3d9627f4dbbd7bc18232
Robert Kotrutsa <tr3asur3island@gmail.com>
2026-04-25 09:24:07 +0200

    Initial commit

---
+++ WeatherNotes/WeatherNotesApp.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by Robert Kotrutsa on 25.04.26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct WeatherNotesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
