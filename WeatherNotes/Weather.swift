14fdca440acadd1ab757ae6221a9dbd4d94797ac
Robert Kotrutsa <tr3asur3island@gmail.com>
2026-04-25 18:43:42 +0200

    feat: add weather model

---
+++ WeatherNotes/Weather.swift
@@ -0,0 +1,12 @@
+//
+//  Weather.swift
+//  WeatherNotes
+//
+//  Created by Robert Kotrutsa on 25.04.26.
+//
+
+struct Weather {
+    let temperature: Double
+    let description: String
+    let icon: String
+}
